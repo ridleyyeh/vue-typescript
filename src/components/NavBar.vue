80bbf90a906e913c95e6771f6da34e9a721a5d5d
ridley ye <ridleyyeh@gmail.com>
2024-05-10 17:42:54 +0800

    Nav and Home

---
+++ src/components/NavBar.vue
@@ -0,0 +1,125 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="navbar">
+        <div class="navbar-left-sec">
+            <a href="">會員中心</a>
+            <a href="">課表查詢</a>
+            <a href="">預約體驗</a>
+        </div>
+        <div class="navbar-title">
+            <a href="" class="title">北投健身房</a>
+        </div>
+
+
+        <div class="navbar-right-sec">
+            <a href="">健身課程</a>
+            <a href="">最近的運動地點</a>
+        </div>
+    </div>
+
+</template>
+
+
+<style scoped lang="scss">
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+
+
+body {
+    position: relative;
+}
+
+
+.navbar {
+
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    width: 100%;
+    top: 0;
+    left: 0;
+    position: absolute;
+    z-index: 1000000;
+    // border: 2px solid rgb(247, 217, 54);
+    height: 5rem;
+
+
+
+
+    .navbar-left-sec {
+
+        width: 350px;
+        display: flex;
+        justify-content: flex-start;
+        gap: 25px;
+        flex: 1;
+        margin-left: 4rem;
+
+
+        a {
+            color: black;
+            font-weight: 900;
+
+
+            &:hover {
+                color: rgb(247, 217, 54);
+                background-color: transparent;
+
+            }
+        }
+    }
+
+    .navbar-title {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 10px;
+
+
+
+        .title {
+            font-size: 32px;
+            color: black;
+            font-weight: 900;
+            text-align: center;
+
+
+            &:hover {
+                color: rgb(247, 217, 54);
+                background-color: transparent;
+
+            }
+
+
+        }
+    }
+
+    .navbar-right-sec {
+
+        display: flex;
+        justify-content: flex-end;
+        flex: 1;
+        gap: 25px;
+        margin-right: 4rem;
+
+
+        a {
+            color: black;
+            font-weight: 900;
+
+            &:hover {
+                color: rgb(247, 217, 54);
+                background-color: transparent;
+
+            }
+        }
+
+
+    }
+}
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import NavBar from '@/components/NavBar.vue';
+</script>
+
+
+<template>
+    <NavBar />
+
+    <div class="content">
+
+    </div>
+
+
+</template>
+
+
+<style scoped lang="scss">
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+html,
+body {
+    overflow-x: hidden
+}
+
+.content {
+    background-image: url('src/assets/workout.jpg');
+    background-size: cover;
+    background-repeat: repeat-x;
+    // max-width: 100%;
+    width: 100vw;
+    height: 100vh;
+    margin-top: 5rem;
+
+
+}
+</style>
